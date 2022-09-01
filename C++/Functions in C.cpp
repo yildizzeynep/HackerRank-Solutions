f366fcda27b9b3593bec935528aad728f74647ce
yildizzeynep <yildizzeynep729@gmail.com>
2022-09-01 18:18:34 +0300

    C++:computer:

---
+++ C++/Functions in C.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+
+    int max_of_four(int a, int b, int c, int d)
+{
+    if (a>b)
+    b=a;
+    if (b>c)
+    c=b;
+    if (c>d)
+    d=c;
+    return d;
+}
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
